cc204f2f595884f78ebc4402f64416d901d552bc
Felipe de Moraes <felipe.moraes@playkids.com>
2021-11-10 22:24:50 -0300

    feat: adjusts on card products, init localstorage

---
+++ src/layout/LayoutDefault.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header company="MagaNets" />
+    <slot />
+    <Footer info="Projeto teste para o Luizalabs realizado por Felipe de Moraes. 2021" />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'LayoutDefault',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
